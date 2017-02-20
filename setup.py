488686aaf516e3f91314f72153ec9faf034c969b
Thiago <thgcode@users.noreply.github.com>
2017-02-20 14:18:42 -0300

    Convert setup script to SetupTools and add more keywords

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 from thdriver.version import SHORT_VERSION
 setup(
     name="thdriver",
     version=SHORT_VERSION,
+url="https://github.com/thgcode/thdriver",
 description="A socket abstraction library and a main loop for servers",
     author="Thiago Seus",
     author_email="thiago.seus@yahoo.com.br",
